88bab830bbf50c01af1c35caa355cdd4d9589dff
s.lukawski@student.utwente.nl <01169205@pw.edu.pl>
2023-06-26 22:02:27 +0200

    Additional Unit tests

--- src/test/java/ut/pp/tests/TestParser.java
+++ src/test/java/ut/pp/tests/TestParser.java
@@ -10,8 +10,8 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.junit.Test;
-import ut.pp.NameNotFoundError;
-import ut.pp.TypeError;
+import Errors.NameNotFoundError;
+import Errors.TypeError;
 
 public class TestParser {
     @Test
@@ -32,6 +32,21 @@ public class TestParser {
         assertEquals(1, tree.getChildCount()); // 1 for Hello, 1 for EOF
     }
     @Test
+    public void testVariableDefinitionSyntaxIncomplete()
+    {
+        String input = "var x = 0 {}";
+        MyLangLexer myLangLexer = new MyLangLexer(CharStreams.fromString(input));
+        CommonTokenStream tokens = new CommonTokenStream(myLangLexer);
+        MyLangParser parser = new MyLangParser(tokens);
+        ParseTree tree = parser.module();
+
+        Visitor visitor = new Visitor();
+        visitor.visit(tree);
+        // TODO Properly test this. ASK TA
+    }
+
+
+    @Test
     public void testJustVeriableDef()
     {
         String input = "var x = 0;";
@@ -46,7 +61,7 @@ public class TestParser {
     }
 
     @Test
-    public void testIfReassignment()
+    public void testReassignment()
     {
         String input = "var x = 0; x = 1;";
         MyLangLexer myLangLexer = new MyLangLexer(CharStreams.fromString(input));
@@ -86,7 +101,123 @@ public class TestParser {
         visitor.visit(tree);
         assertTrue(visitor.error_vector.get(0) instanceof TypeError);
     }
+    @Test
+    public void testTypeErrorBool()
+    {
+        String input = "var x = True; x = 1;";
+        MyLangLexer myLangLexer = new MyLangLexer(CharStreams.fromString(input));
+        CommonTokenStream tokens = new CommonTokenStream(myLangLexer);
+        MyLangParser parser = new MyLangParser(tokens);
+        ParseTree tree = parser.module();
 
+        Visitor visitor = new Visitor();
+        visitor.visit(tree);
+        assertTrue(visitor.error_vector.get(0) instanceof TypeError);
+    }
+    @Test
+    public void testManyNestedScopes()
+    {
+        String input = "var x = True; {x = False; var y = 2; {var z = 3; z = 0;} y = 10;} x = True;";
+        MyLangLexer myLangLexer = new MyLangLexer(CharStreams.fromString(input));
+        CommonTokenStream tokens = new CommonTokenStream(myLangLexer);
+        MyLangParser parser = new MyLangParser(tokens);
+        ParseTree tree = parser.module();
+
+        Visitor visitor = new Visitor();
+        visitor.visit(tree);
+        assertEquals(0, visitor.error_vector.size());
+    }
+    @Test
+    public void testManyNestedScopesError()
+    {
+        String input = "var x = True; {x = False; var y = 2; {var z = 3; z = 0;} y = 10;} z = 1;";
+        MyLangLexer myLangLexer = new MyLangLexer(CharStreams.fromString(input));
+        CommonTokenStream tokens = new CommonTokenStream(myLangLexer);
+        MyLangParser parser = new MyLangParser(tokens);
+        ParseTree tree = parser.module();
+
+        Visitor visitor = new Visitor();
+        visitor.visit(tree);
+        assertTrue(visitor.error_vector.get(0) instanceof NameNotFoundError);
+    }
+    @Test
+    public void testScopes_and_type_error_order()
+    {
+        String input = "var x = True; {x = False; var y = 2; {var z = 3; z = 0;} y = 10;} z = False;";
+        MyLangLexer myLangLexer = new MyLangLexer(CharStreams.fromString(input));
+        CommonTokenStream tokens = new CommonTokenStream(myLangLexer);
+        MyLangParser parser = new MyLangParser(tokens);
+        ParseTree tree = parser.module();
+
+        Visitor visitor = new Visitor();
+        visitor.visit(tree);
+        assertTrue(visitor.error_vector.get(0) instanceof NameNotFoundError);
+    }
+    @Test
+    public void testAssignmentPlusEq()
+    {
+        String input = "var x = 0; x += 1;";
+        MyLangLexer myLangLexer = new MyLangLexer(CharStreams.fromString(input));
+        CommonTokenStream tokens = new CommonTokenStream(myLangLexer);
+        MyLangParser parser = new MyLangParser(tokens);
+        ParseTree tree = parser.module();
+
+        Visitor visitor = new Visitor();
+        visitor.visit(tree);
+        assertEquals(0, visitor.error_vector.size());
+    }
+    @Test
+    public void testAssignmentMinusEq()
+    {
+        String input = "var x = 0; x -= 1;";
+        MyLangLexer myLangLexer = new MyLangLexer(CharStreams.fromString(input));
+        CommonTokenStream tokens = new CommonTokenStream(myLangLexer);
+        MyLangParser parser = new MyLangParser(tokens);
+        ParseTree tree = parser.module();
+
+        Visitor visitor = new Visitor();
+        visitor.visit(tree);
+        assertEquals(0, visitor.error_vector.size());
+    }
+    @Test
+    public void testAssignmentWithUnknownOperation()
+    {
+        String input = "var x = 0; x ?= 1;";
+        MyLangLexer myLangLexer = new MyLangLexer(CharStreams.fromString(input));
+        CommonTokenStream tokens = new CommonTokenStream(myLangLexer);
+        MyLangParser parser = new MyLangParser(tokens);
+        ParseTree tree = parser.module();
+
+        Visitor visitor = new Visitor();
+        visitor.visit(tree);
+        // TODO Properly test this. ASK TA
+    }
+    @Test
+    public void testRelation()
+    {
+        String input = "var a = True; var b = False; var c = a <= b;";
+        MyLangLexer myLangLexer = new MyLangLexer(CharStreams.fromString(input));
+        CommonTokenStream tokens = new CommonTokenStream(myLangLexer);
+        MyLangParser parser = new MyLangParser(tokens);
+        ParseTree tree = parser.module();
+
+        Visitor visitor = new Visitor();
+        visitor.visit(tree);
+        assertEquals(0, visitor.error_vector.size());
+    }
+    @Test
+    public void testManyRelations()
+    {
+        String input = "var a = True; var b = False; var c = a <= b > True < False;";
+        MyLangLexer myLangLexer = new MyLangLexer(CharStreams.fromString(input));
+        CommonTokenStream tokens = new CommonTokenStream(myLangLexer);
+        MyLangParser parser = new MyLangParser(tokens);
+        ParseTree tree = parser.module();
+
+        Visitor visitor = new Visitor();
+        visitor.visit(tree);
+        assertEquals(0, visitor.error_vector.size());
+    }
 
 
 }
