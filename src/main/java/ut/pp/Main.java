9befc90b8918db0406636a9c8007997fd3d1c436
Mateusz <midec.p@gmail.com>
2023-06-27 15:46:35 +0200

    Update main

--- src/main/java/ut/pp/Main.java
+++ src/main/java/ut/pp/Main.java
@@ -1,24 +1,14 @@
-//package ut.pp;
-//
-//import org.antlr.v4.runtime.CommonTokenStream;
-//import org.antlr.v4.runtime.CharStreams;
-//import org.antlr.v4.runtime.tree.ParseTree;
-//
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        System.out.println("Hello, Programming Paradigms");
-//
-//        String input = "// Line one\n"
-//            + "Hello\n"
-//            + "// Line two\n"
-//            + "Hello Hello\n";
-//
-//        MyLangLexer myLangLexer = new MyLangLexer(CharStreams.fromString(input));
-//        CommonTokenStream tokens = new CommonTokenStream(myLangLexer);
-//        MyLangParser parser = new MyLangParser(tokens);
-//        ParseTree tree = parser.hellos();
-//
+package ut.pp;
+
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Programming Paradigms");
+
 //        System.out.println("Children: " + tree.getChildCount() + ", parsed text: " + tree.getText());
-//    }
-//}
+    }
+}
