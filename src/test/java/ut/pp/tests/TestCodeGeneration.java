3ab22ee865912759bb115c3653484d5b0e305620
Mateusz <midec.p@gmail.com>
2023-07-05 08:56:14 +0200

    Add code generation test

--- src/test/java/ut/pp/tests/TestCodeGeneration.java
+++ src/test/java/ut/pp/tests/TestCodeGeneration.java
@@ -13,7 +13,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestCodeGeneration {
-     Visitor visitor;
+    Visitor visitor;
 
     /**
      * Generates machine code
@@ -33,59 +33,205 @@ public class TestCodeGeneration {
         String epilogue = ", WriteInstr reg0 (DirAddr 0), EndProg]";
         return prologue + text + epilogue;
     }
+
+    String petersonCode = "module Main where \n" +
+            "\n" +
+            "import Sprockell \n" +
+            "\n" +
+            "prog0 = [Load (ImmValue 1) regA\n" +
+            "      , WriteInstr regA (DirAddr 0)\n" +
+            "      , Load (ImmValue 0) regA\n" +
+            "      , Push regA\n" +
+            "      , WriteInstr regA (DirAddr 1)\n" +
+            "      , Load (ImmValue 0) regA\n" +
+            "      , Push regA\n" +
+            "      , WriteInstr regA (DirAddr 2)\n" +
+            "      , Load (ImmValue 3) regA\n" +
+            "      , Push regA\n" +
+            "      , Load (ImmValue 1) regA\n" +
+            "      , WriteInstr regA (DirAddr 3)\n" +
+            "      , Pop regA\n" +
+            "      , Store regA (DirAddr 0)\n" +
+            "      , Load (ImmValue 4) regA\n" +
+            "      , Push regA\n" +
+            "      , Load (ImmValue 1) regA\n" +
+            "      , WriteInstr regA (DirAddr 4)\n" +
+            "      , Pop regA\n" +
+            "      , Store regA (DirAddr 1)\n" +
+            "      , Load (DirAddr 0) regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regA\n" +
+            "      , ReadInstr (IndAddr regA)\n" +
+            "      , Receive regB\n" +
+            "      , Branch regB (Rel (-2))\n" +
+            "      , Load (DirAddr 1) regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regA\n" +
+            "      , ReadInstr (IndAddr regA)\n" +
+            "      , Receive regB\n" +
+            "      , Branch regB (Rel (-2))\n" +
+            "      , ReadInstr (DirAddr 2)\n" +
+            "      , Receive regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regA\n" +
+            "      , WriteInstr regA numberIO\n" +
+            "      , WriteInstr regA (DirAddr 0)\n" +
+            "      , EndProg]\n" +
+            "\n" +
+            "prog1 = [ReadInstr (DirAddr 3)\n" +
+            "      , Receive regA\n" +
+            "      , Compute Equal regA reg0 regA\n" +
+            "      , Branch regA (Rel (-3))\n" +
+            "      , Load (ImmValue 15) regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regA\n" +
+            "      , Store regA (DirAddr 0)\n" +
+            "      , Load (DirAddr 0) regA\n" +
+            "      , Push regA\n" +
+            "      , Load (ImmValue 0) regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regB\n" +
+            "      , Pop regA\n" +
+            "      , Compute Gt regA regB regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regA\n" +
+            "      , Compute Equal regA reg0 regA\n" +
+            "      , Branch regA (Abs 50)\n" +
+            "      , Load (ImmValue 1) regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regA\n" +
+            "      , WriteInstr regA numberIO\n" +
+            "      , Load (DirAddr 0) regA\n" +
+            "      , Push regA\n" +
+            "      , Load (ImmValue 1) regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regB\n" +
+            "      , Pop regA\n" +
+            "      , Compute Sub regA regB regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regA\n" +
+            "      , Store regA (DirAddr 0)\n" +
+            "      , TestAndSet (DirAddr 1)\n" +
+            "      , Receive regA\n" +
+            "      , Compute Equal regA reg0 regA\n" +
+            "      , Branch regA (Rel (-3))\n" +
+            "      , ReadInstr (DirAddr 2)\n" +
+            "      , Receive regA\n" +
+            "      , Push regA\n" +
+            "      , Load (DirAddr 0) regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regB\n" +
+            "      , Pop regA\n" +
+            "      , Compute Add regA regB regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regA\n" +
+            "      , WriteInstr regA (DirAddr 2)\n" +
+            "      , WriteInstr reg0 (DirAddr 1)\n" +
+            "      , Jump (Abs 8)\n" +
+            "      , Load (ImmValue 999) regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regA\n" +
+            "      , WriteInstr regA numberIO\n" +
+            "      , WriteInstr reg0 (DirAddr 3)\n" +
+            "      , EndProg]\n" +
+            "\n" +
+            "prog2 = [ReadInstr (DirAddr 4)\n" +
+            "      , Receive regA\n" +
+            "      , Compute Equal regA reg0 regA\n" +
+            "      , Branch regA (Rel (-3))\n" +
+            "      , Load (ImmValue 15) regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regA\n" +
+            "      , Store regA (DirAddr 0)\n" +
+            "      , Load (DirAddr 0) regA\n" +
+            "      , Push regA\n" +
+            "      , Load (ImmValue 0) regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regB\n" +
+            "      , Pop regA\n" +
+            "      , Compute Gt regA regB regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regA\n" +
+            "      , Compute Equal regA reg0 regA\n" +
+            "      , Branch regA (Abs 50)\n" +
+            "      , Load (ImmValue 2) regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regA\n" +
+            "      , WriteInstr regA numberIO\n" +
+            "      , Load (DirAddr 0) regA\n" +
+            "      , Push regA\n" +
+            "      , Load (ImmValue 1) regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regB\n" +
+            "      , Pop regA\n" +
+            "      , Compute Sub regA regB regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regA\n" +
+            "      , Store regA (DirAddr 0)\n" +
+            "      , TestAndSet (DirAddr 1)\n" +
+            "      , Receive regA\n" +
+            "      , Compute Equal regA reg0 regA\n" +
+            "      , Branch regA (Rel (-3))\n" +
+            "      , ReadInstr (DirAddr 2)\n" +
+            "      , Receive regA\n" +
+            "      , Push regA\n" +
+            "      , Load (DirAddr 0) regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regB\n" +
+            "      , Pop regA\n" +
+            "      , Compute Add regA regB regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regA\n" +
+            "      , WriteInstr regA (DirAddr 2)\n" +
+            "      , WriteInstr reg0 (DirAddr 1)\n" +
+            "      , Jump (Abs 8)\n" +
+            "      , Load (ImmValue 1000) regA\n" +
+            "      , Push regA\n" +
+            "      , Pop regA\n" +
+            "      , WriteInstr regA numberIO\n" +
+            "      , WriteInstr reg0 (DirAddr 4)\n" +
+            "      , EndProg]\n" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "main = run [prog0,prog1,prog2]";
     @Before
     public void setup() {
         visitor = new Visitor();
     }
 
-//    @Test
-//    public void varDefinitionTid() {
-//        String text = "var x = Tid;";
-//
-//        generateCode(text);
-//        assertEquals(fullCode("Load (ImmValue 0) regA, Store regA (DirAddr 0)"), visitor.getCode(0));
-//    }
-//
-//    @Test
-//    public void varDefinitionWithAddition() {
-//        String text = "var x = 2 + 2;";
-//
-//        generateCode(text);
-//        assertEquals(fullCode("Load (ImmValue 2) regA, Load (ImmValue 2) regB, " +
-//                "Compute Add regA regB regB, Store regB (DirAddr 0)"),visitor.getCode(0));
-//    }
-//    @Test
-//    public void printStatement() {
-//        String text = "print 2 + 2";
-//
-//        generateCode(text);
-//        assertEquals(fullCode("Load (ImmValue 2) regA, Load (ImmValue 2) regB," +
-//                " Compute Add regA regB regB, WriteInstr regB numberIO"),visitor.getCode(0));
-//    }
-//    @Test
-//    public void additionInExpressionStatementThenPrint() {
-//        String text = "var x = 2; x = x + 2; print x;";
-//
-//        generateCode(text);
-//        assertEquals(fullCode("Load (ImmValue 2) regA, Store regA (DirAddr 0), Load (DirAddr 0) regA, Load (ImmValue 2) regB," +
-//                " Compute Add regA regB regB, Store regB (DirAddr 0), Load (DirAddr 0) regA," +
-//                " WriteInstr regA numberIO"), visitor.getCode(0));
-//    }
-//    @Test
-//    public void additionInPrint() {
-//        String text = "var x = 2; print x + 3";
-//
-//        generateCode(text);
-//        assertEquals(fullCode("Load (ImmValue 2) regA, Store regA (DirAddr 0), Load (DirAddr 0) regA, Load (ImmValue 3) regB," +
-//                " Compute Add regA regB regB, WriteInstr regB numberIO"), visitor.getCode(0));
-//    }
-//    @Test
-//    public void varToVarAssignment() {
-//        String text = "var x = 2; var y = x + 1;";
-//
-//        generateCode(text);
-//        System.out.println(  CodeGenerator.prettyCodeWithLineNumbers(visitor.getCode(0)));
-//        assertEquals(fullCode("Load (ImmValue 2) regA, Store regA (DirAddr 0), Load (DirAddr 0) regA, Load (ImmValue 1) regB," +
-//                " Compute Add regA regB regB, Store regB (DirAddr 1)"), visitor.getCode(0));
-//    }
+    @Test
+    public void petersonCode() throws Exception {
+        String peterson = "shared var x = False;\n" +
+                "shared var bank = 0;\n" +
+                "var t1 = fork {\n" +
+                "    var i = 15;\n" +
+                "    while i > 0\n" +
+                "    {\n" +
+                "        print 1;\n" +
+                "        i = i - 1;\n" +
+                "        lock x;\n" +
+                "        bank = bank + i;\n" +
+                "        unlock x;\n" +
+                "    }\n" +
+                "    print 999;\n" +
+                "};\n" +
+                "\n" +
+                "var t2 = fork {\n" +
+                "    var i = 15 ;\n" +
+                "    while i > 0\n" +
+                "    {\n" +
+                "        print 2;\n" +
+                "        i = i - 1;\n" +
+                "        lock x;\n" +
+                "        bank = bank + i;\n" +
+                "        unlock x;\n" +
+                "    }\n" +
+                "    print 1000;\n" +
+                "};\n" +
+                "join t1;\n" +
+                "join t2;\n" +
+                "print bank;";
+        assertEquals(petersonCode, CodeGenerator.generateCode(peterson, false));
+    }
 }
